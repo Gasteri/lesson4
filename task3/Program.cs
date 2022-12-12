5590f69a8145805fbef7a149ae742d88f5e5a4f4
Gasteri <tipzov@yandex.ru>
2022-12-12 11:26:45 +0300

    add factorial

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* int Factorial(int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.WriteLine(Factorial(5));*/
+
+/*int Factorial(int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for(int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}*/
+
+
+double Factorial(int n) //тип double помогает от переполнения ячеек! пропадают отрицательные числа!27! = 1,0888869450418352E+28 +28(10 в 28-й степени)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for(int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
